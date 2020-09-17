51534915da24ac2b85b635160c9adb58cf40b138
Ethan-23 <emanroberts@gmail.com>
2020-09-17 16:38:55 +0000

    Removed non c programs

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - This will display numbers 0-9
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
