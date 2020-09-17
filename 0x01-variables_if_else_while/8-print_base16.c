4f71fafaa43cb0b869547efd7192fd9bfe5cd842
Ethan-23 <emanroberts@gmail.com>
2020-09-17 17:46:27 +0000

    First Commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - displays 0-9 then a-f
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 48; i < 58; i++)
+		putchar(i);
+	for (i = 'a'; i < 'f'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
