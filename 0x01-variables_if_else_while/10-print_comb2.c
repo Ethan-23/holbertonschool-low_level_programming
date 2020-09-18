b065a1da0a212a5a8577576b081ff3bbc68b6064
Ethan-23 <emanroberts@gmail.com>
2020-09-18 02:04:20 +0000

    First Commit

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -7,6 +7,20 @@
 int main(void)
 {
 	int i;
-
-	return (0);
+	int t;
+	for (t = 48; t < 58; t++)
+	{
+		for (i = 48; i < 58; i++)
+		{
+			putchar(t);
+			putchar(i);
+			if (i <= 57)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+return (0);
 }
