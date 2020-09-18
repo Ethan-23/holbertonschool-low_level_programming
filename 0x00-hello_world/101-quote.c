6fafe2d877cf6c774019cf045a1a92be99a40e91
Ethan-23 <emanroberts@gmail.com>
2020-09-18 16:42:00 +0000

    First Commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - This will display the text below
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
+	return (0);
+}
