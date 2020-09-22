74f7b504d85192db7a2204cd98dcec550de6a162
Ethan-23 <emanroberts@gmail.com>
2020-09-22 17:52:55 +0000

    Changed if statement

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,16 +10,11 @@ int print_last_digit(int f)
 {
 	int g;
 
-	if (f < 0)
+	g = f % 10;
+	if (g < 0)
 	{
-		f = f * -1;
-		g = f % 10;
-		_putchar('0' + g);
+		g = g * -1;
 	}
-	else
-	{
-		g = f % 10;
-		_putchar('0' + g);
-	}
+	_putchar('0' + g);
 	return (g);
 }
