08c9e1f82cf4d7b86fd5be639a1a6505e89ac824
Ethan-23 <emanroberts@gmail.com>
2020-09-22 13:23:04 +0000

    First Commit

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * int_islower - This will tell you if a character is lower or not
+ *
+ * Return: 0
+ */
+int int_islower(int c)
+{
+	if (c >= 97)
+	{
+		_putchar(48);
+	}
+	else
+	{
+		_putchar(49);
+	}
+}
