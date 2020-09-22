ce0e4d362b15a299b390431f34245d1699992cf0
Ethan-23 <emanroberts@gmail.com>
2020-09-22 23:45:12 +0000

    First Commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * add - adds 2 intigers
+ * @fd: test
+ * @sd: test
+ * Return: Always 0.
+ */
+int add(int fd, int sd)
+{
+	int a;
+
+	a = fd + sd;
+	return (a);
+}
