21f60868e3d0150c80771c766bbdd4f72fe84032
Ethan-23 <emanroberts@gmail.com>
2020-09-24 13:14:50 +0000

    Updated code to have <= and > =

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,14 +2,18 @@
 #include <stdio.h>
 
 /**
- * main - check the code for Holberton School students.
- *
+ * _isupper - checks if letter stored in c is uppercase
+ * @c: letter being checked
  * Return: Always 0.
  */
 int _isupper(int c)
 {
-	if (c > 'A' && c < 'Z')
-		return (1)
-		else
-			return (0)
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+			return (0);
+	}
 }
