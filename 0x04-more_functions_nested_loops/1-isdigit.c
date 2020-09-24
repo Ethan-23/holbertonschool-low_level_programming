283b5c2ec7384433fb5fa6b4d4d54d100f8c6fca
Ethan-23 <emanroberts@gmail.com>
2020-09-24 16:55:17 +0000

    Betty solved

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,8 +2,8 @@
 #include <stdio.h>
 
 /**
- * main - check the code for Holberton School students.
- *
+ * _isdigit - check the code for Holberton School students.
+ * @c: number input
  * Return: Always 0.
  */
 int _isdigit(int c)
