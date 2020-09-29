afcd06fe6112deee7713658ec4dcfb54512e1a20
Ethan-23 <emanroberts@gmail.com>
2020-09-29 13:14:16 +0000

    First Commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strlen - check the code for Holberton School students.
+ * @s: string length
+ * Return: Always 0.
+ */
+
+int _strlen(char *s)
+{
+	char b = strlen(s);
+
+	return (b);
+}
