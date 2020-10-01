3c7cdeefbd1634ad3980ecb4804fd47cc70a200e
Ethan-23 <emanroberts@gmail.com>
2020-10-01 14:27:21 +0000

    First Commit

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -2,8 +2,10 @@
 #include <stdio.h>
 
 /**
- * main - check the code for Holberton School students.
- *
+ * _strncat - check the code for Holberton School students.
+ * @dest: Hello
+ * @src: World!
+ * @n: Number of char
  * Return: Always 0.
  */
 
@@ -15,11 +17,11 @@ char *_strncat(char *dest, char *src, int n)
 	while (dest[count] != '\0')
 		count++;
 	while (count2 < n && src[count2] != '\0')
-        {
+	{
 		dest[count] = src[count2];
 		count2++;
 		count++;
-        }
+	}
 	dest[count] = '\0';
 	return (dest);
 }
