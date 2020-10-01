1aec03ae1e7c33116a2aac6748c7f0f331f19383
Ethan-23 <emanroberts@gmail.com>
2020-10-01 14:59:39 +0000

    First Commit

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int count = 0;
+
+	while (count < n && src[count] != '\0')
+	{
+		dest[count] = src[count];
+		count++;
+	}
+	while (count < n)
+	{
+		dest[count] = '\0';
+		count++;
+	}
+	return (dest);
+}
