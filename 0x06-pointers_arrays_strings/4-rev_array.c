c123d75dc3384ee5a68d97aad9bffdb8139ee763
Ethan-23 <emanroberts@gmail.com>
2020-10-01 18:04:55 +0000

    First Commit

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * reverse_array - check the code for Holberton School students.
+ * @a: an array of integers
+ * @n: the number of elements to swap
+ *
+ * Return: nothing.
+ */
+void reverse_array(int *a, int n)
+{
+	int count = 0;
+	int count2 = 0;
+	int hold = 0;
+
+	while (a[count] < a[n])
+	{
+		count++;
+	}
+	while (count > count2)
+	{
+		count--;
+		hold = a[count2];
+		a[count2] = a[count];
+		a[count] = hold;
+		count2++;
+	}
+}
