8842966119f44d62a712fc207c04b9d5505855ff
Ethan-23 <emanroberts@gmail.com>
2020-10-02 18:20:14 +0000

    First Commit

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+char *leet(char *a)
+{
+	char b[] = "aeotlAEOTL";
+	char z[] = "4307143071";
+	int rep = 0;
+	int count = 0;
+
+	while (a[count] != '\0')
+	{
+		rep = 0;
+		while (b[rep] != '\0')
+		{
+			if (a[count] == b[rep])
+				a[count] = z[rep];
+			rep++;
+		}
+		count++;
+	}
+	return (a);
+}
