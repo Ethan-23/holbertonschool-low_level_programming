8af53d61fafad0a5e8398f35cded4c9ba971871c
Ethan-23 <emanroberts@gmail.com>
2020-10-02 17:59:22 +0000

    First Commit

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+char *cap_string(char *a)
+{
+	int test = 0;
+	int count = 0;
+	char *t = " \t\n,;.!?\"(){}";
+
+	if (a[0] >= 'a' && a[0]<= 'z')
+		a[0] -= 32;
+		while (a[count] != '\0')
+		{
+			test = 0;
+			while (t[test] != '\0')
+			{
+				if (a[count - 1] == t[test] && a[count] >= 'a' && a[count] <= 'z')
+				{
+					a[count] -= 32;
+				}
+				test++;
+			}
+			count++;
+		}
+	return (a);
+}
