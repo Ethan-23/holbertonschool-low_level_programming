37bdefe2510aeecc233142b8ae560a0b6582c521
Ethan-23 <emanroberts@gmail.com>
2020-10-05 13:59:19 +0000

    First Commit

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * simple_print_buffer - prints buffer in hexa
+ * @buffer: the address of memory to print
+ * @size: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	for (; n > 0; n--)
+		dest[n] = src[n];
+	return (dest);
+}
