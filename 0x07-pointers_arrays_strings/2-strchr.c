474f8e4bcd91c1737d7aff111aee1e3760eb7bfb
Ethan-23 <emanroberts@gmail.com>
2020-10-05 14:15:48 +0000

    First Commit

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+char *_strchr(char *s, char c)
+{
+	for (; *s != '\0'; s++)
+	{
+		if (*s == c)
+		return (s);
+	}
+	return (s);
+}
