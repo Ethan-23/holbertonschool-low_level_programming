8c61ad8b82e5b6d474cf105abc21e88aa54a09e1
Ethan-23 <emanroberts@gmail.com>
2020-10-05 19:13:45 +0000

    First Commit

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_chessboard - check the code for Holberton School students.
+ * @a: chess
+ * Return: Always 0.
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 7; i++)
+	{
+		for (j = 0; j <= 7; j++)
+		{
+			_putchar(a[i][j]);
+		}
+			_putchar('\n');
+	}
+}
