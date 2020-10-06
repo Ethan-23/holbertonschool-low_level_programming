1451872efd3b8a8f52ca950e617c2251609b5f9c
Ethan-23 <emanroberts@gmail.com>
2020-10-06 18:13:40 +0000

    First Commit

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - check the code for Holberton School students.
+ * @a: test
+ * @size: test
+ * Return: Always 0.
+ */
+void print_diagsums(int *a, int size)
+{
+	int fsize = size * size;
+	int i;
+	int j;
+	int sum;
+	int sum2;
+
+	for (i = 0; i < fsize; i += size + 1)
+		sum += a[i];
+	for (j = size * size - 1; j > 0; j -= size - 1)
+		sum2 += a[j];
+	printf("%d, %d", sum, sum2);
+}
