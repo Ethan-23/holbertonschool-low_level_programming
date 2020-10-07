7f2810b8a9d261e9e30ead61d0c09472affbc88e
Ethan-23 <emanroberts@gmail.com>
2020-10-07 18:45:48 +0000

    First Commit

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * count - check the code for Holberton School students.
+ * _sqrt_recursion - output function
+ * @a: multiplication num
+ * @n: given value
+ * Return: Always 0.
+ */
+int count(int a, int n)
+{
+	if (a * a == n)
+		return (a);
+	if (a * a > n)
+		return (-1);
+	return (count(a + 1, n));
+}
+int _sqrt_recursion(int n)
+{
+	return (count(1, n));
+}
