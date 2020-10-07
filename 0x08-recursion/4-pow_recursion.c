a52057de9cef6cee306240245b40c68ed454070d
Ethan-23 <emanroberts@gmail.com>
2020-10-07 14:59:26 +0000

    First Commit

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _pow_recursion - check the code for Holberton School students.
+ * @x: Number input
+ * @y: Exponent input
+ * Return: Always 0.
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y < 1)
+		return (1);
+	else
+		return x * (_pow_recursion(x, y - 1));
+}
