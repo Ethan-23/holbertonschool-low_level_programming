e1384187b94539d562c7b8b62480fc4592243855
Ethan-23 <emanroberts@gmail.com>
2020-10-11 22:29:57 +0000

    Subtracted by 1 and betty changes

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,13 +2,13 @@
 #include <stdlib.h>
 
 /**
- *
- *
- *
- *
+ * main - prints number of args
+ * @argc: test
+ * @argv: test
+ * Return: 0
  **/
 int main(int argc, char *argv[] __attribute__((unused)))
 {
-	printf ("%d\n", argc);
+	printf("%d\n", argc - 1);
 		return (0);
 }
