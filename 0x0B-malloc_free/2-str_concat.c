60dbd54cb30ceca8d287e22f115557d963f58ab9
Ethan-23 <emanroberts@gmail.com>
2020-10-13 15:05:15 +0000

    First Commit

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * str_concat - cats 2 strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: 0
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *ar;
+	unsigned int j;
+	unsigned int i;
+
+	ar = malloc(sizeof(s1) + sizeof(s2) + 1);
+	if (ar == NULL)
+		return (NULL);
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		ar[i] = s1[i];
+	}
+	for (j = 0; s2[j] != '\0'; j++)
+	{
+		ar[i] = s2[j];
+		i++;
+	}
+	return (ar);
+}
