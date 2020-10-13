7d136ac839441724cddaa3460806ddf31b7a75ff
Ethan-23 <emanroberts@gmail.com>
2020-10-13 14:11:11 +0000

    FIrst Commit

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * create_array - checking size and setting to null
+ * @size: size of char
+ * @c: string
+ * Return: 0
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *ar;
+
+	ar = malloc(size * sizeof(char));
+	if (ar == 0)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		ar[i] = c;
+	return (ar);
+}
