7e198586bc9b20cee08f1366ebed389d2e82f697
Ethan-23 <emanroberts@gmail.com>
2020-10-13 16:32:50 +0000

    Changed if

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,9 +10,10 @@ char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
 	char *ar;
-
+	if (size == 0)
+		return (NULL);
 	ar = malloc(size * sizeof(char));
-	if (ar == 0)
+	if (ar == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
 		ar[i] = c;
