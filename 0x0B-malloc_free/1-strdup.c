be46bcc35aa0966a802421e934a5f29dc1c287b2
Ethan-23 <emanroberts@gmail.com>
2020-10-13 17:54:56 +0000

    added length func

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -5,17 +5,27 @@
  * @str: something
  * Return: 0
  */
+int str_leng(char *a)
+{
+	int i;
+	for (i = 0; a[i] != '\0'; i++)
+	{
+
+	}
+	return (i);
+}
 char *_strdup(char *str)
 {
 	char *ar;
 	int i;
+	int add = str_leng(str);
 
 	if (str == NULL)
 		return (NULL);
-	ar = malloc(sizeof(str));
-	if (ar < str)
+	ar = malloc(sizeof(add) * sizeof(char));
+	if (ar == 0)
 		return (NULL);
 	for (i = 0; str[i] != '\0'; i++)
-	ar[i] = str[i];
+		ar[i] = str[i];
 	return (ar);
 }
