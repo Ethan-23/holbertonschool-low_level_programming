599011298cdc0e562c0ea96bc2b88254b4346a5c
Ethan-23 <emanroberts@gmail.com>
2020-10-13 19:02:33 +0000

    First Commit

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+/**
+ * alloc_grid - makes a grid
+ * @width: width
+ * @height: height
+ * Return: 0
+ */
+int **alloc_grid(int width, int height)
+{
+	int **ar;
+	int i;
+	int j;
+	int b;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	ar = (int **)malloc(width * height * sizeof(int));
+	for (b = 0; b < width; b++)
+		ar[b] = (int *)malloc(height * sizeof(int));
+	if (ar == NULL)
+		return (NULL);
+	for (i = 0; i <= height; i++)
+		for (j = 0; j < width; j++)
+			ar[i][j] = 0;
+	return (ar);
+}
