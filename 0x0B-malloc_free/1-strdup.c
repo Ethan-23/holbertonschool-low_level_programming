01ac4e325960d3684a503085eda47f47fb7481e6
Ethan-23 <emanroberts@gmail.com>
2020-10-13 16:47:33 +0000

    Changed from size to null

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -9,15 +9,13 @@ char *_strdup(char *str)
 {
 	char *ar;
 	int i;
-	int size;
 
 	ar = malloc(sizeof(str));
 	if (ar == NULL)
 		return (NULL);
-	size = sizeof(str)/sizeof(char);
 	if (ar < str)
 		return (NULL);
-	for (i = 0; i <= size; i++)
+	for (i = 0; str[i] != '\0'; i++)
 	ar[i] = str[i];
 	return (ar);
 }
