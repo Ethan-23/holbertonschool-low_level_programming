545869720004398d7ab67bd63f1595b242a8387c
Ethan-23 <emanroberts@gmail.com>
2020-10-20 02:59:15 +0000

    New struc

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,10 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 int _putchar(char c);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 /**
  * struct dog - Short description
  * @name: First member
@@ -15,4 +19,5 @@ struct dog
 	float age;
 	char *owner;
 };
+typedef struct dog dog_t;
 #endif
