ab4f96da6b14752b24304610bcb4bed0e6e096cf
Ethan-23 <emanroberts@gmail.com>
2020-10-20 02:25:55 +0000

    Betty Description added

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 int _putchar(char c);
+/**
+ * struct dog - Short description
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
 struct dog
 {
 	char *name;
