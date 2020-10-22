785ca9d23c879dd333ad85b4d284046e0e1a0bc2
Ethan-23 <emanroberts@gmail.com>
2020-10-22 18:47:07 +0000

    Added code and betty

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -2,9 +2,18 @@
 #include "function_pointers.h"
 
 /**
- * print_elem - prints an integer
- * @elem: the integer to print
- *
+ * array_iterator - prints an integer
+ * @array: the integer to print
+ * @size: size
+ * @action: action
  * Return: Nothing.
  */
-void array_iterator(int *array, size_t size, void (*action)(int));
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	if (array == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
