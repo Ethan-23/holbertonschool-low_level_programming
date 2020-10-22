3df53b9d0cc6171aacd246e1616bc6044596def4
Ethan-23 <emanroberts@gmail.com>
2020-10-22 03:19:04 +0000

    First Commit

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name_as_is - prints a name as is
+ * @name: name of the person
+ * @f: function
+ * @c: char
+ * Return: Nothing.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+
+}
