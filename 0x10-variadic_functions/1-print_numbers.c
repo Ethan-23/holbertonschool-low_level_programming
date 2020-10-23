1feec62bc04d9cb00715ad191efac336e10dbb69
Ethan-23 <emanroberts@gmail.com>
2020-10-23 16:50:48 +0000

    First Commit

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * print_numbers - check the code for Holberton School students.
+ * @separator: seperator string
+ * @n: Number of numbers
+ * Return: Always 0.
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list num;
+
+	va_start(num, n);
+	if (separator != NULL)
+	{
+		for (i = 0; i < n; i++)
+		{
+			printf("%d", va_arg(num, int));
+			if (i < n - 1)
+				printf("%s", separator);
+		}
+		printf("\n");
+		va_end(num);
+	}
+}
