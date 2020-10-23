f71806792b468532155bbc1d11820d6c2ff89245
Ethan-23 <emanroberts@gmail.com>
2020-10-23 15:14:03 +0000

    betty changes

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -3,8 +3,8 @@
 #include <stdarg.h>
 
 /**
- * main - check the code for Holberton School students.
- *
+ * sum_them_all - check the code for Holberton School students.
+ * @n: number of numbers
  * Return: Always 0.
  */
 int sum_them_all(const unsigned int n, ...)
@@ -15,9 +15,9 @@ int sum_them_all(const unsigned int n, ...)
 
 	if (n == 0)
 		return (0);
-	va_start (given, n);
+	va_start(given, n);
 	for (i = 0; i < n; i++)
-		sum += va_arg (given, int);
-	va_end (given);
+		sum += va_arg(given, int);
+	va_end(given);
 	return (sum);
 }
