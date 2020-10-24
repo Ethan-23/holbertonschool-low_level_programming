31f78c491488910ac5f56aad7d2c58686f3a6e41
Ethan-23 <emanroberts@gmail.com>
2020-10-24 18:42:23 +0000

    changed if statment to prevent seperator instead of entire line

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,15 +13,13 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list num;
 
 	va_start(num, n);
-	if (separator != NULL)
-	{
 		for (i = 0; i < n; i++)
 		{
 			printf("%d", va_arg(num, int));
 			if (i < n - 1)
+				if (separator != NULL)
 				printf("%s", separator);
 		}
 		va_end(num);
-	}
 	printf("\n");
 }
