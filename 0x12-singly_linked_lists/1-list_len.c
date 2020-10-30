f0272141d2a451a5947b27bd62c73cbff0a5f626
Ethan-23 <emanroberts@gmail.com>
2020-10-30 02:35:35 +0000

    first commit

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * add_node - check the code for Holberton School students.
+ * @h: list
+ * Return: Always 0.
+ */
+size_t list_len(const list_t *h)
+{
+	int charnum;
+
+	for (charnum = 0; h != NULL; charnum++)
+		h = h->next;
+	return (charnum);
+}
