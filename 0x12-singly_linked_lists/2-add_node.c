9ba6989ff65961d4f3712e78060f07e9ba7663e0
Ethan-23 <emanroberts@gmail.com>
2020-10-30 03:06:51 +0000

    Changed lines around and added description for betty

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -4,15 +4,16 @@
 #include "lists.h"
 
 /**
- * main - check the code for Holberton School students.
- *
+ * add_node - check the code for Holberton School students.
+ * @head: list
+ * @str: strings
  * Return: Always 0.
  */
 list_t *add_node(list_t **head, const char *str)
 {
 	int charnum;
-
 	list_t *new_list = malloc(sizeof(list_t));
+
 	if (new_list == NULL)
 		return (NULL);
 	for (charnum = 0; str[charnum] != '\0'; charnum++)
