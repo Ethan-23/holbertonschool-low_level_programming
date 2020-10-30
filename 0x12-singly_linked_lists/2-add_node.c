ceab0c6f1bd58b58a47f1e20585cf44df9458221
Ethan-23 <emanroberts@gmail.com>
2020-10-30 03:03:23 +0000

    First Commit

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	int charnum;
+
+	list_t *new_list = malloc(sizeof(list_t));
+	if (new_list == NULL)
+		return (NULL);
+	for (charnum = 0; str[charnum] != '\0'; charnum++)
+	{}
+	new_list->str = strdup(str);
+	new_list->len = charnum;
+	new_list->next = (*head);
+	(*head) = new_list;
+		return (new_list);
+}
