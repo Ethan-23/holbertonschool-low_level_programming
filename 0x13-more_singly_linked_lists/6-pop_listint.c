51a2a2a94f464c1c17f7cd82d38b3fe91473cd7a
Ethan-23 <emanroberts@gmail.com>
2020-11-04 03:09:46 +0000

    First Commit

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * pop_listint - check the code for Holberton School students.
+ * @head: given list
+ * Return: Always 0.
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+
+	if (head == NULL)
+		return (0);
+	temp = *head;
+	*head = (*head)->next;
+	free(temp);
+
+	return ((*head)->n - 1);
+}
