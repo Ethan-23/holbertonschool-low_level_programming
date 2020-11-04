2ea31cb3e1716b0b663d3d4aa3e51b464bcbc5df
Ethan-23 <emanroberts@gmail.com>
2020-11-04 01:34:30 +0000

    First Commit

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_listint - check the code for Holberton School students.
+ * @head: given list
+ * Return: Always 0.
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head != NULL)
+	{
+	temp = head;
+	head = head->next;
+	free(temp);
+	}
+}
