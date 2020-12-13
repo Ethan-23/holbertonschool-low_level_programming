3c17a8312112ccf935bbb2ca724eead859294b9d
Ethan-23 <emanroberts@gmail.com>
2020-12-13 23:34:09 +0000

    First Commit

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * dlistint_t - check the code for Holberton School students.
+ * @h: given var
+ * Return: Always 0.
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	int i;
+
+	for (i = 0; h != NULL; i++)
+	{
+		h = h->next;
+	}
+	return (i);
+}
