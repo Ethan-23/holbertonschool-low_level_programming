142ff21864d1104affb105fdf9db29553f1cb97e
Ethan-23 <emanroberts@gmail.com>
2021-01-15 22:09:37 +0000

    First Commit

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+/**
+ * hash_table_set - thing
+ * @ht: hash table
+ * @key: key
+ * @value: value associated with key
+ * Return: 1 if succeed or 0 if not
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned int index;
+	hash_node_t *newnode;
+
+	if (key == NULL || ht == NULL || ht->size == 0 || ht->array == NULL)
+		return 0;
+	index = hash_djb2((unsigned char *)key) % ht->size;
+	newnode = malloc(sizeof(hash_node_t));
+	if (newnode == NULL)
+		return 0;
+	newnode->key = strdup(key);
+	newnode->value = strdup(value);
+	newnode->next = NULL;
+	if (ht->array[index] == NULL)
+		ht->array[index] = newnode;
+	else
+	{
+		if (strcmp(ht->array[index]->key, key) == 0)
+			ht->array[index]->value = strdup(value);
+		else
+		{
+			newnode->next = ht->array[index];
+			ht->array[index] = newnode;
+		}
+	}
+	return(1);
+}
