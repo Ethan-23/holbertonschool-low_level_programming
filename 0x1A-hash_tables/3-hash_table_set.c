3859117cd48edcde8e87b1abb8be413b83f44730
Ethan-23 <emanroberts@gmail.com>
2021-01-15 22:11:00 +0000

    betty changes

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -15,11 +15,11 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *newnode;
 
 	if (key == NULL || ht == NULL || ht->size == 0 || ht->array == NULL)
-		return 0;
+		return (0);
 	index = hash_djb2((unsigned char *)key) % ht->size;
 	newnode = malloc(sizeof(hash_node_t));
 	if (newnode == NULL)
-		return 0;
+		return (0);
 	newnode->key = strdup(key);
 	newnode->value = strdup(value);
 	newnode->next = NULL;
@@ -35,5 +35,5 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			ht->array[index] = newnode;
 		}
 	}
-	return(1);
+	return (1);
 }
