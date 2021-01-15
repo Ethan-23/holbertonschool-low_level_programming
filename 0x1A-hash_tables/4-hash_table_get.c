1265c7dd4d87914e1bc45f29256fbaf6c53572a9
Ethan-23 <emanroberts@gmail.com>
2021-01-15 22:34:48 +0000

    First Commit

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - check the code for Holberton School students.
+ * @ht: hash table
+ * @key: key
+ * Return: value of element or null of key is not found
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned int index;
+	hash_node_t *find;
+
+	if (key == NULL || ht == NULL)
+		return (NULL);
+	index = hash_djb2((unsigned char *)key) % ht->size;
+	find = ht->array[index];
+	while (find != NULL)
+	{
+		if (strcmp(find->key, key) == 0)
+			return (find->value);
+		find = find->next;
+	}
+	return(NULL);
+}
