7ded9b2470e22b7d915973a4667cb9959aa3d381
Ethan-23 <emanroberts@gmail.com>
2021-02-20 03:42:50 +0000

    Updated with documentation

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
+"""island checker checks for characters that differ around others"""
+
+
 def island_perimeter(grid):
+    """Checks for water(0) around land(1)"""
+
     count = 0
     for i in range(len(grid)):
         for g in range(len(grid[i])):
             if i is not 0:
-                if grid[i-1][g] == 0 and grid[i-1][g] != grid [i][g]:
+                if grid[i-1][g] == 0 and grid[i-1][g] != grid[i][g]:
                     count += 1
-                if grid[i-1][g] == 1 and grid[i-1][g] != grid [i][g]:
+                if grid[i-1][g] == 1 and grid[i-1][g] != grid[i][g]:
                     count += 1
                 if g+1 < len(grid[i]):
                     if grid[i][g] == 1 and grid[i][g] != grid[i][g+1]:
